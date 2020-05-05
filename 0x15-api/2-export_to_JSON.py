38a2a0d777b7f122cf8a9239cb46458857129ffe
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-05 01:42:26 -0500

    [ADD]2. Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,42 +1,35 @@
 #!/usr/bin/python3
-""" Program that Gather data from an API """
+""" Program that Gather data from an API and Export to JSON """
+import json
+import requests
+from sys import argv
+
 if __name__ == "__main__":
-    import json
-    import requests
-    from sys import argv
     """ Program Entry point """
     empId = argv[1]
-    int_id = int(empId)
-    url_todo = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-        int_id)
-    url_user = 'https://jsonplaceholder.typicode.com/users?id={}'.format(
-        int_id)
+    url_todo = 'https://jsonplaceholder.typicode.com/todos'
+    url_user = 'https://jsonplaceholder.typicode.com/users'
+    payload1 = {'userId': empId}
+    payload2 = {'id': empId}
 
-    req_todo = requests.get(url_todo)
-    req_user = requests.get(url_user)
+    req_todo = requests.get(url_todo, params=payload1)
+    req_user = requests.get(url_user, params=payload2)
 
     # Getting the NUMBER_OF_DONE_TASKS and total tasks
     total_tasks = req_todo.json()
-    done_tasks = []
-    for task in total_tasks:
-        if (task.get('completed') is True):
-            done_tasks.append(task)
-
     # Employee name from users
     user_data = req_user.json()
-    emp_name = user_data[0].get('name')
-    for js in total_tasks:
-        title_task = js.get('title')
-        completed_task = js.get('completed')
+    emp_name = user_data[0].get('username')
+    list_dict = []
+    user_tasks = {}
 
-    list_json = []
-
-    dic = {"task": title_task,
-           "completed": completed_task,
-           "username": emp_name}
-    list_json.append(dic)
-
-    data_id = {int_id: list_json}
-
-    with open('{}.json'.format(int_id), 'w') as file_json:
-        json.dump(data_id, file_json)
+    with open('{}.json'.format(empId), 'w') as json_file:
+        for task in total_tasks:
+            task_info = {}
+            task_info['task'] = task.get('title')
+            task_info['completed'] = task.get('completed')
+            task_info['username'] = emp_name
+            list_dict.append(task_info)
+        user_tasks[empId] = list_dict
+        info = json.dumps(user_tasks)
+        json_file.write(info)

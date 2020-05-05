55bcfbe69076e6a0bfbd6cb4ac234d29ad8b75c2
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-04 23:08:24 -0500

    [ADD]1. Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Program that Gather data from an API """
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    """ Program Entry point """
+    empId = argv[1]
+    int_id = int(empId)
+    url_todo = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        int_id)
+    url_user = 'https://jsonplaceholder.typicode.com/users?id={}'.format(
+        int_id)
+
+    req_todo = requests.get(url_todo)
+    req_user = requests.get(url_user)
+
+    # Getting the NUMBER_OF_DONE_TASKS and total tasks
+    total_tasks = req_todo.json()
+    done_tasks = []
+    for task in total_tasks:
+        if (task.get('completed') is True):
+            done_tasks.append(task)
+
+    # Employee name from users
+    user_data = req_user.json()
+    emp_name = user_data[0].get('name')
+
+    with open('{}.csv'.format(int_id), 'w') as file_task:
+        for write in total_tasks:
+            file_task.write('"{}","{}","{}","{}"\n'.format(
+                int_id, emp_name, write.get('completed'), write.get('title')))

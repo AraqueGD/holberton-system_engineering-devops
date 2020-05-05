8a78007ac16adf8e0c288923d0d383fdd3d9e6e5
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-05 01:43:33 -0500

    [ADD]2. Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
-""" Program that Gather data from an API and Export to JSON """
-import json
-import requests
-from sys import argv
-
+""" Program that Gather data from an API """
 if __name__ == "__main__":
+    import json
+    import requests
+    from sys import argv
     """ Program Entry point """
     empId = argv[1]
-    url_todo = 'https://jsonplaceholder.typicode.com/todos'
-    url_user = 'https://jsonplaceholder.typicode.com/users'
-    payload1 = {'userId': empId}
-    payload2 = {'id': empId}
+    int_id = int(empId)
+    url_todo = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        int_id)
+    url_user = 'https://jsonplaceholder.typicode.com/users?id={}'.format(
+        int_id)
 
-    req_todo = requests.get(url_todo, params=payload1)
-    req_user = requests.get(url_user, params=payload2)
+    req_todo = requests.get(url_todo)
+    req_user = requests.get(url_user)
 
     # Getting the NUMBER_OF_DONE_TASKS and total tasks
     total_tasks = req_todo.json()
+    done_tasks = []
+    for task in total_tasks:
+        if (task.get('completed') is True):
+            done_tasks.append(task)
+
     # Employee name from users
     user_data = req_user.json()
-    emp_name = user_data[0].get('username')
+    emp_name = user_data[0].get('name')
+    for js in total_tasks:
+        title_task = js.get('title')
+        completed_task = js.get('completed')
+
     list_dict = []
     user_tasks = {}
 

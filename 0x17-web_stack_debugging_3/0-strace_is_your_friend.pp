159202772793201b4163640ec5d003b11dae0a2d
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-21 10:24:06 -0500

    [UPD]Comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet (instead of using Bash as you were previously doing).
 exec {'typo error in /var/www/html/wp-settings.php file name':
   path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
   command => 'mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp'
